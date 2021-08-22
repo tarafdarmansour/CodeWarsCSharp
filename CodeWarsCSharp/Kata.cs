6fa2ea7de3e7cf12b68aec280712c18bfe623a73
tarafdarmansour <tarafdar.mansour@gmail.com>
2021-08-22 18:19:03 +0400

    Likes Method

---
+++ CodeWarsCSharp/Kata.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWarsCSharp
+{
+    public static class Kata
+    {
+        public static string Likes(string[] name)
+        {
+            if (name.Length == 0)
+                return $"no one likes this";
+            else if (name.Length == 1)
+                return $"{name[0]} likes this";
+            else if (name.Length == 2)
+                return $"{name[0]} and {name[1]} like this";
+            else if (name.Length == 3)
+                return $"{name[0]}, {name[1]} and {name[2]} like this";
+
+            return $"{name[0]}, {name[1]} and {name.Length - 2} others like this";
+        }
+    }
+}
---
+++ CodeWarsCSharp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CodeWarsCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            // ---- Likes Test
+            string[] arr = new string[] { "John", "Jane" , "Davies", "Taylor", "Smith"};
+            Console.WriteLine(Kata.Likes(arr));
+        }
+    }
+
+    
+}
